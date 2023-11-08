247ba83b9c14cd5da0cd3cadd339e790d1fe55b0
Mohammad Moien <mnmoienian@gmail.com>
2023-11-08 21:47:46 +0330

    add validation to edit-form component

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -14,10 +14,10 @@ import { useFormState } from 'react-dom';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
-  const [state, despatch] = useFormState(createInvoice, initialState);
+  const [state, dispatch] = useFormState(createInvoice, initialState);
 
   return (
-    <form action={despatch}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
